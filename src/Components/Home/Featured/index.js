fbd36da8ad6f91a41a3b7b70669e8532873545d8
Devin Blankenship <failure2fly@gmail.com>
2021-01-11 22:50:47 -0700

    added new folders, sub-folders, and files. Header, Footer, Featured, Hoc, UI. Header and Footer is created. Got stripes for Featured

---
+++ src/Components/Home/Featured/index.js
@@ -0,0 +1,13 @@
+import React from  'react';
+import Stripes from './stripes';
+
+const Featured = (props) => {
+  return (
+    <div className="featured_wrapper">
+        <Stripes/>
+    </div>
+  );
+};
+
+export default Featured;
+
