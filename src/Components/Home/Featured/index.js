5c28a66cc215008547ea3f28fce2f64f2ebb83c4
Devin Blankenship <failure2fly@gmail.com>
2021-01-13 00:02:52 -0700

    Finished with changes for Featured. Added Text. Added Number. Added Image.

--- src/Components/Home/Featured/index.js
+++ src/Components/Home/Featured/index.js
@@ -1,10 +1,12 @@
 import React from  'react';
 import Stripes from './stripes';
+import Text from './text';
 
 const Featured = (props) => {
   return (
     <div className="featured_wrapper">
         <Stripes/>
+        <Text/>
     </div>
   );
 };
